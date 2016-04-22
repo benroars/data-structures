4a06656dc8e19c49b841a0b75382b9d2ea69f14a
benroars <benihanas1@gmail.com>
2016-04-21 18:21:51 -0700

    Adding implementation for set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,27 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if (!this._storage.hasOwnProperty(item)) {
+    this._storage[item] = 1;
+  }
 };
 
 setPrototype.contains = function(item) {
+  if (this._storage[item]) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 setPrototype.remove = function(item) {
+  delete this._storage[item];
 };
 
 /*
