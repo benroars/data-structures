4e9b2a5a64d738ce2e981c5b0315ecf7ea82940e
benroars <benihanas1@gmail.com>
2016-04-21 22:43:07 -0700

    Adding implementation for hashtable.js

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,5 +1,3 @@
-
-
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
@@ -7,14 +5,58 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var contents = this._storage.get(index);
+
+  if (!contents) {
+    this._storage.set(index, []);
+  }
+  var keyval = [k, v];
+
+  var temp = this._storage.get(index);
+  
+  if (this._storage.get(index)[0] !== undefined) {  
+    for (var i = 0; i < this._storage.get(index).length; i++) {
+      if (this._storage.get(index)[i][0] === k) {
+        console.log('suh');
+      }
+    }
+  }
+
+  temp.push(keyval);
+  this._storage.set(index, temp);
+
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var contents = this._storage.get(index);
+  var results = '';
+
+  for (var i = 0; i < contents.length; i++) {
+    if (contents[i][0] === k) {
+      results = contents[i][1];
+    }
+  }
+
+  return results;
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  
+  var temp = this._storage.get(index);
+ // console.log(temp[0]);
+
+  for (var i = 0; i < temp.length; i++) {
+    if (temp[i][0] === k) {
+      temp[i][1] = undefined;
+    }
+  }
+//  console.log(temp[0]);
+
+
+  this._storage.set(index, temp);
+
 };
 
 
